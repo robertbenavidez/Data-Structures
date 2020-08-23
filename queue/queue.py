a614aa1dac777551e3f2cb4346104125e79bc9d8
Robert Benavidez <murabitbenavidez@gmail.com>
2020-08-23 13:23:21 -0400

    singly linked list

--- queue/queue.py
+++ queue/queue.py
@@ -13,22 +13,43 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-import sys
+# import sys
+#
+# sys.path.append('../doubly_linked_list')
+# from doubly_linked_list import DoublyLinkedList
+#
+#
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = DoublyLinkedList()
+#
+#     def __len__(self):
+#         return self.storage.length
+#
+#     def enqueue(self, value):
+#         self.storage.add_to_tail(value)
+#
+#     def dequeue(self):
+#         return self.storage.remove_from_head()
 
-sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
-
-
-class Queue:
-    def __init__(self):
-        self.size = 0
-        self.storage = DoublyLinkedList()
-
-    def __len__(self):
-        return self.storage.length
-
-    def enqueue(self, value):
-        self.storage.add_to_tail(value)
-
-    def dequeue(self):
-        return self.storage.remove_from_head()
+# SLL Version
+# import sys
+#
+# sys.path.append('../singly_linked_list')
+# from singly_linked_list import LinkedList
+#
+#
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+#
+#     def __len__(self):
+#         return self.storage.length
+#
+#     def enqueue(self, value):
+#         self.storage.add_to_tail(value)
+#
+#     def dequeue(self):
+#         return self.storage.remove_head()
