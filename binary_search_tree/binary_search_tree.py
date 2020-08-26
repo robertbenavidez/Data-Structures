f2f146b7b45cf839d701bafd836c734478ca6eea
Robert Benavidez <murabitbenavidez@gmail.com>
2020-08-25 21:14:45 -0400

    BST contains function

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -35,7 +35,20 @@ class BSTNode:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+
+        elif target > self.value:
+            if self.right is not None:
+                return self.right.contains(target)
+            else:
+                return False
+        else:
+            if self.left is not None:
+                return self.left.contains(target)
+            else:
+                return False
+
 
     # Return the maximum value found in the tree
     def get_max(self):
